b0e553fb279e210e48bc4dd8efdf8941c1eef806
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-07-13 22:55:22 -0700

    Add pkg-config

--- scripts/cloudlab_startup_run_by_creator.sh
+++ scripts/cloudlab_startup_run_by_creator.sh
@@ -5,7 +5,7 @@
 #
 
 sudo apt update
-sudo apt -y install software-properties-common
+sudo apt -y install software-properties-common pkg-config
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible
 ansible-galaxy collection install ansible.posix
